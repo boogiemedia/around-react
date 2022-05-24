76a87b44b1fad3cf51bd27f7ec95db66a52b7c45
boogiemedia <speedysokol@gmail.com>
2022-05-24 21:23:43 +0300

    sdf

---
+++ src/components/Card.js
@@ -0,0 +1,33 @@
+import React, {useState} from "react"
+export default function Card(props){
+  const [isOpen, setIsOpen] = useState(false);
+    function HandleCardClick(){
+        setIsOpen(!isOpen)
+    }
+    function HandleCloseButton() {
+      setIsOpen(false);
+    }
+    const {name , link , likes } = props.item
+    console.log(link)
+    return(
+      <>
+        <div className="elements__block" onClick = {HandleCardClick}>
+          <button className="elements__trash"></button>
+          <div className="elements__cover" style={{backgroundImage: `url(${link})` }}></div>
+          <h2 className="elements__title">{name}</h2>
+          <div className="elements__like-box">
+            <button type="button" class="elements__like"></button>
+          <p className="elements__like-counter">{likes.length}</p>
+          </div>
+        </div>
+            <div className={isOpen  ? `popup popup__preview popup_oppened` : `popup popup__preview`  }>  
+            <div className="popup__preview-box">
+              <button type="button" className="popup__close-button" aria-label="close-button" onClick={HandleCloseButton}></button>
+              <img src= {link} alt={name} className="popup__preview"/>
+              <p className="popup__preview-text">{name}</p>
+            </div>
+          </div>
+        
+    </>
+    )
+}
---
+++ src/components/Main.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+import Popins from "../components/PopupWithForm";
+import Card from "./Card";
+import api from "../blocks/utils/api";
+
+export default function Main() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [callpPopup, setCallpopup] = useState("");
+  const [userCards, setUserCards] = useState([]);
+  const [userProfile, setUserProfile] = useState({});
+
+  async function getAPIData() {
+    const data_cards = await api.getCards();
+    const dataProfile = await api.getProfile();
+    setUserCards(data_cards);
+    setUserProfile(dataProfile);
+  }
+  useEffect(() => {
+    console.log("adasdasas");
+    getAPIData();
+  }, []);
+
+  async function popupOpener(ev) {
+    ev.preventDefault();
+    setCallpopup(ev.target.id);
+    setIsOpen(!isOpen);
+  }
+  function HandleCloseButton() {
+    setIsOpen(false);
+  }
+  return (
+    <main className="main">
+      <section className="profile">
+        <div className="profile__avatar-cover">
+          <div
+            id="change-profile-picture"
+            onClick={popupOpener}
+            className="profile__avatar-middle"
+          ></div>
+          <img
+            src={userProfile.avatar}
+            id="profile-avatar"
+            alt="profile-avatar"
+            className="profile__avatar"
+          />
+          <div className="profile__avatar-button"></div>
+        </div>
+
+        <div className="profile__info-block">
+          <h1 className="profile__info">{userProfile.name}</h1>
+          <button
+            id="profile-edditor"
+            onClick={popupOpener}
+            type="button"
+            className="profile__edit-button"
+            aria-label="edit-button"
+          ></button>
+          <p className="profile__sub-info">{userProfile.about}</p>
+        </div>
+
+        <button
+          id="card-editor"
+          onClick={popupOpener}
+          type="button"
+          className="profile__add-button"
+          aria-label="add-button"
+        ></button>
+      </section>
+      <section className="elements">
+        <Popins
+          isOpen={isOpen}
+          cssClassName={callpPopup}
+          closePopUp={HandleCloseButton}
+        />
+        {userCards.map((ele) => {
+          return <Card item={ele} />;
+        })}
+      </section>
+    </main>
+  );
+}
