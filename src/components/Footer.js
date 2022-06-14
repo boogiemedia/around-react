651926a47ae222c738311c8b5f74b0baae6713f4
boogiemedia <speedysokol@gmail.com>
2022-06-14 13:37:07 +0300

    date added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 import React from "react";
 export default function Footer() {
+  const date = new Date().getFullYear()
   return (
     <footer className="footer">
-      <p className="footer__title">© 2021 Around The U.S.</p>
+      <p className="footer__title">© {date} Around The U.S.</p>
     </footer>
   );
 }
